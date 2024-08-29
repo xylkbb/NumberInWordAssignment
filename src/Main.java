4ea2ec1716513879190a2ae0de484f06a14e8e33
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-29 12:55:18 -0400

    Commited

--- src/Main.java
+++ src/Main.java
@@ -17,10 +17,10 @@ public class Main {
 
     public static void printNumberInWord(int number) {
 
-        //Enhanced Version
+        // Enhanced Version
         switch (number) {
-        case 0 -> System.out.println("ZERO");
-        case 1 -> System.out.println("ONE");
+            case 0 -> System.out.println("ZERO");
+            case 1 -> System.out.println("ONE");
             case 2 -> System.out.println("TWO");
             case 3 -> System.out.println("THREE");
             case 4 -> System.out.println("FOUR");
@@ -29,7 +29,7 @@ public class Main {
             case 7 -> System.out.println("SEVEN");
             case 8 -> System.out.println("EIGHT");
             case 9 -> System.out.println("NINE");
-        default -> System.out.println("OTHER");
+            default -> System.out.println("OTHER");
         }
     }
 
