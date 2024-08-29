237e309b2884df31eef3f8b635eff49ace02b426
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-29 12:54:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(2);
+        printNumberInWord(3);
+        printNumberInWord(4);
+        printNumberInWord(5);
+        printNumberInWord(6);
+        printNumberInWord(7);
+        printNumberInWord(8);
+        printNumberInWord(9);
+        printNumberInWord(10);
+        printNumberInWord(-1);
+    }
+
+    public static void printNumberInWord(int number) {
+
+        //Enhanced Version
+        switch (number) {
+        case 0 -> System.out.println("ZERO");
+        case 1 -> System.out.println("ONE");
+            case 2 -> System.out.println("TWO");
+            case 3 -> System.out.println("THREE");
+            case 4 -> System.out.println("FOUR");
+            case 5 -> System.out.println("FIVE");
+            case 6 -> System.out.println("SIX");
+            case 7 -> System.out.println("SEVEN");
+            case 8 -> System.out.println("EIGHT");
+            case 9 -> System.out.println("NINE");
+        default -> System.out.println("OTHER");
+        }
+    }
+
+}
